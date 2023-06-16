b5f8abb69e8cb22028ab61fd5d51760009750ff0
Silas Morales <silas.mora22@gmail.com>
2023-06-15 18:44:37 -0700

    updated some css

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     
-    <div className="max-w-5xl rounded-lg shadow-lg items-center justify-center md:flex md:mx-8 md:mt-24 md:bg-white md:h-[600px] lg:mx-auto ">
+    <div className="max-w-4xl rounded-lg shadow-lg items-center justify-center md:flex md:mx-8 md:mt-24 md:bg-white md:h-[600px] lg:mx-auto ">
 
       <Sidebar 
       totalSteps={totalSteps}
